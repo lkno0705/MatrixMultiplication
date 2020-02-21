e2eeb256d36c8727e09ede1cbb7ef98ef761183f
i516998 <leon.knorr@sap.com>
2020-02-21 12:35:46 +0100

    change Matrix length to 2840x2840

--- MatrixMultiplikation_C++/MatrixMultiplication/Matrix.h
+++ MatrixMultiplikation_C++/MatrixMultiplication/Matrix.h
@@ -4,8 +4,8 @@
 namespace Matrix {
 	class matrix {
 	public:
-		const static int n = 1440;
-		const static int m = 1440;
+		const static int n = 2840;
+		const static int m = 2840;
 		std::vector<std::vector<long>> matrix;
 		void createRandomMatrix();
 		void print();
