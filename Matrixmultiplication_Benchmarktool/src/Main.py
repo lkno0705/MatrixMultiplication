cbfb8bee8729225de9e5872bfaebb3b142880630
lkno0705 <lkno0705@gmail.com>
2020-03-03 20:48:19 +0100

    bla

--- Matrixmultiplication_Benchmarktool/src/Main.py
+++ Matrixmultiplication_Benchmarktool/src/Main.py
@@ -1,7 +1,11 @@
 import subprocess
 
+# python cmd: "python", "E:/Programs/MatrixMultiplication-master/MatrixMultiplication_Python/Matrixmult.py"
+# CUDA cmd: "E:/Programs/MatrixMultiplication-master/MatrixMultiplication_Exec/MatrixMultiplication_CUDA.exe"
+# C cmd: "E:/Programs/MatrixMultiplication-master/MatrixMultiplication_Exec/MatrixMultiplication_C.exe"
+
 def run():
-    cmd = ["python", "E:/Programs/MatrixMultiplication-master/MatrixMultiplication_Python/Matrixmult.py"]
+    cmd = ["E:/Programs/MatrixMultiplication-master/MatrixMultiplication_Exec/MatrixMultiplication_CUDA.exe"]
     out = subprocess.Popen(cmd, stdout=subprocess.PIPE ).communicate()[0]
     print(out)
 
--- Matrixmultiplication_Benchmarktool/src/MarkdownCreation.py
+++ Matrixmultiplication_Benchmarktool/src/MarkdownCreation.py
@@ -1,15 +1,17 @@
 
 class Readme:
 
-    def newReadme(self):
-        path = "E:/Programs/MatrixMultiplication-master/README.md"
+    def newReadme(self, data):
+        path = "E:/Programs/MatrixMultiplication-master/Matrixmultiplication_Benchmarktool/README.md"
         file = open(path, "w")
 
+        langs = ["Python", "Java","C++", "CUDA", "Kotlin", "Rust", "C#", "C", "GO"]
+        langs.sort()
         header = "# MatrixMultiplication" \
                  " Just a little playground, to test and try the benefits of Running Calculations on CPU or GPU with multiple threads. " \
                  "\n\n" \
                  "Compared Runtimes:\n\n" \
                  "Matrix size: 1440x1440 <br> " \
                  "Threads: 24\n\n"
-        
+
 
