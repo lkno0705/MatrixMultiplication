00bdee742902d8db95a6140b5f1fd8ec3c46daf9
lkno0705 <lkno0705@gmail.com>
2020-03-03 21:35:33 +0100

    add terminal output conversion to benchmarktool

--- Matrixmultiplication_Benchmarktool/src/Main.py
+++ Matrixmultiplication_Benchmarktool/src/Main.py
@@ -4,9 +4,31 @@ import subprocess
 # CUDA cmd: "E:/Programs/MatrixMultiplication-master/MatrixMultiplication_Exec/MatrixMultiplication_CUDA.exe"
 # C cmd: "E:/Programs/MatrixMultiplication-master/MatrixMultiplication_Exec/MatrixMultiplication_C.exe"
 
-def run():
-    cmd = ["E:/Programs/MatrixMultiplication-master/MatrixMultiplication_Exec/MatrixMultiplication_CUDA.exe"]
-    out = subprocess.Popen(cmd, stdout=subprocess.PIPE ).communicate()[0]
-    print(out)
 
-run()
+def run():
+    cmd = ["E:/Programs/MatrixMultiplication-master/MatrixMultiplication_Exec/MatrixMultiplication_Go.exe"]
+    out = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+    out = str(out).replace("\\n", " ")
+    out = out.replace("\\r", " ")
+    out = out.replace("ms", " ")
+    out = out.replace("s", " ")
+    out = out.replace(",", ".")
+    print(out)
+    return out
+
+
+def convertOutput(out):
+    data = []
+    for i in out.split():
+        try:
+            data.append(int(i))
+        except ValueError:
+            try:
+                data.append(float(i))
+            except ValueError:
+                pass
+    print(data)
+
+
+out = run()
+convertOutput(out)
