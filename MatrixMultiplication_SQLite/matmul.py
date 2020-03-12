b134812542441395da01053e07bbbec0f0bde460
imkgerC <imrund08@gmail.com>
2020-03-12 15:10:39 +0100

    optimize startup speed of sqlite

--- MatrixMultiplication_SQLite/matmul.py
+++ MatrixMultiplication_SQLite/matmul.py
@@ -6,18 +6,18 @@ def main():
     n = 1440
     
     conn = sqlite3.connect(":memory:")
-    cur = conn.cursor()
 
-    cur.execute("CREATE TABLE a (row INTEGER, col INTEGER, val REAL);")
-    cur.execute("CREATE TABLE b (row INTEGER, col INTEGER, val REAL);")
+    conn.execute("CREATE TABLE a (row INTEGER, col INTEGER, val REAL);")
+    conn.execute("CREATE TABLE b (row INTEGER, col INTEGER, val REAL);")
 
-    for i in range(n):
-        for j in range(n):
-            cur.execute("INSERT INTO a (row, col, val) VALUES (?, ?, ?)", (i, j, random.random()))
-            cur.execute("INSERT INTO b (row, col, val) VALUES (?, ?, ?)", (i, j, random.random()))
+    with conn:
+        for i in range(n):
+            for j in range(n):
+                conn.execute("INSERT INTO a (row, col, val) VALUES (?, ?, ?)", (i, j, random.random()))
+                conn.execute("INSERT INTO b (row, col, val) VALUES (?, ?, ?)", (i, j, random.random()))
     
     start = time.time()
-    cur.execute("SELECT b.row, a.col, SUM(a.val*b.val) FROM a INNER JOIN b on a.row = b.col GROUP BY a.col, b.row ;")
+    conn.execute("SELECT b.row, a.col, SUM(a.val*b.val) FROM a INNER JOIN b on a.row = b.col GROUP BY a.col, b.row ;")
     end = time.time()
     print(f"took {end-start}s")
 
